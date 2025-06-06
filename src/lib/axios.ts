5c6f02185dbf470434c66035484ba0d1a86fe379
daanielsss <danielameric2112o@gmail.com>
2025-06-06 01:41:22 -0600

    Comit 6

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL + "/api", // Asegúrate que esto da: https://consh.../api
 });
 
 // Interceptor para incluir el token en cada request
