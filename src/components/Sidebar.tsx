1aa4a61fa3f8597907c82fd5c8239ebdac8eb628
daanielsss <danielameric2112o@gmail.com>
2025-08-05 20:54:29 -0600

    Commit 96

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -55,7 +55,7 @@ export default function Sidebar() {
             {/* Botón flotante para abrir sidebar */}
             {!expanded && (
                 <button
-                    className="fixed top-4 left-4 z-50 bg-primary/30 backdrop-blur-md border border-white/10 text-white rounded-full p-2 shadow-md"
+                    className="fixed top-4 left-4 z-50 bg-primary/30 blur-md border border-white/10 text-white rounded-full p-2 shadow-md"
 
                     onClick={toggleSidebar}
                 >
@@ -73,7 +73,7 @@ export default function Sidebar() {
 
             {/* Sidebar con slide universal */}
             <aside
-                className={`fixed top-0 left-0 h-screen w-56 z-40 bg-primary/30 backdrop-blur-md border-r border-white/10 shadow-xl text-primary-foreground flex flex-col
+                className={`fixed top-0 left-0 h-screen w-56 z-40 bg-primary/30 blur-md border-r border-white/10 shadow-xl text-primary-foreground flex flex-col
       transform transition-transform duration-300
       ${expanded ? "translate-x-0" : "-translate-x-full"}
   `}
